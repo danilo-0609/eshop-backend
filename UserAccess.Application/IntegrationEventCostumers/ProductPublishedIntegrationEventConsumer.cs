e7ca7ba65d79c9824dafd501c4264fb45c83f2f0
Danilo Usuga <daniusugaj@gmail.com>
2024-01-31 18:07:51 -0500

    Adding logging service

--- UserAccess.Application/IntegrationEventCostumers/ProductPublishedIntegrationEventConsumer.cs
+++ UserAccess.Application/IntegrationEventCostumers/ProductPublishedIntegrationEventConsumer.cs
@@ -1,6 +1,6 @@
 ﻿using Catalog.IntegrationEvents;
 using MassTransit;
-using MediatR;
+using Microsoft.Extensions.Logging;
 using UserAccess.Domain;
 using UserAccess.Domain.Users;
 
@@ -9,21 +9,31 @@ namespace UserAccess.Application.IntegrationEventCostumers;
 public sealed class ProductPublishedIntegrationEventConsumer : IConsumer<ProductPublishedIntegrationEvent>
 {
     private readonly IUserRepository _userRepository;
+    private readonly ILogger<ProductPublishedIntegrationEventConsumer> _logger;
 
-    public ProductPublishedIntegrationEventConsumer(IUserRepository userRepository)
+    public ProductPublishedIntegrationEventConsumer(IUserRepository userRepository, ILogger<ProductPublishedIntegrationEventConsumer> logger)
     {
         _userRepository = userRepository;
+        _logger = logger;
     }
 
     public async Task Consume(ConsumeContext<ProductPublishedIntegrationEvent> context)
     {
+        _logger.LogInformation("Start consuming: {Name}, {DateTime}",
+            context.GetType().FullName,
+            DateTime.UtcNow);
+
         var userId = context.Message.SellerId;
 
-        var user = await _userRepository.GetByIdAsync(UserId.Create(userId));
+        var roles = await _userRepository.GetRolesAsync(userId);
 
-        if (!user!.Roles.Any(r => r.RoleCode == "Seller"))
+        if (roles!.Any(r => r.RoleCode != Role.Seller.RoleCode))
         {
-            await _userRepository.AddRole(user.Id.Value, Role.Seller);
+            await _userRepository.AddRole(userId, Role.Seller);
         }
+
+        _logger.LogInformation("Consuming finished: {Name}, {DateTime}",
+            context.GetType().FullName,
+            DateTime.UtcNow);
     }
 }
