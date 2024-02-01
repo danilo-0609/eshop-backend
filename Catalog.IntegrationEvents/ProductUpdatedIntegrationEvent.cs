3cceff9842a9b4835ac017936f3d6c5d9455b73b
Danilo Usuga <daniusugaj@gmail.com>
2024-02-01 17:30:37 -0500

    Improving integration event structure

--- Catalog.IntegrationEvents/ProductUpdatedIntegrationEvent.cs
+++ Catalog.IntegrationEvents/ProductUpdatedIntegrationEvent.cs
@@ -4,4 +4,8 @@ namespace Catalog.IntegrationEvents;
 public record ProductUpdatedIntegrationEvent(
     Guid IntegrationEventId,
     Guid ProductId,
+    string Name,
+    Guid SellerId,
+    decimal Price,
+    int InStock,
     DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
