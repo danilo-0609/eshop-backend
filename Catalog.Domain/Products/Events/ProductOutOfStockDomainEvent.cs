b039728c89148016dac068fbff23cdc948a4b6c0
Danilo Usuga <daniusugaj@gmail.com>
2024-02-02 20:58:01 -0500

    Fixing class structure

--- Catalog.Domain/Products/Events/ProductOutOfStockDomainEvent.cs
+++ Catalog.Domain/Products/Events/ProductOutOfStockDomainEvent.cs
@@ -1,6 +1,7 @@
 using BuildingBlocks.Domain;
 
 namespace Catalog.Domain.Products.Events;
+
 public sealed record ProductOutOfStockDomainEvent(Guid DomainEventId,
     ProductId ProductId,
     DateTime OcurredOn) : IDomainEvent;
