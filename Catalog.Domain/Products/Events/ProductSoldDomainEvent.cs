b11b5814c8f668f93918fa6d6852cd6a97f9c1aa
Danilo Usuga <daniusugaj@gmail.com>
2024-02-03 20:52:52 -0500

    Fixing class structure

--- Catalog.Domain/Products/Events/ProductSoldDomainEvent.cs
+++ Catalog.Domain/Products/Events/ProductSoldDomainEvent.cs
@@ -2,6 +2,7 @@ using System;
 using BuildingBlocks.Domain;
 
 namespace Catalog.Domain.Products.Events;
+
 public record ProductSoldDomainEvent(Guid DomainEventId,
     ProductId ProductId,
     int Amount,
--- Catalog.Domain/Sales/Events/SaleGeneratedDomainEvent.cs
+++ Catalog.Domain/Sales/Events/SaleGeneratedDomainEvent.cs
@@ -2,6 +2,7 @@ using BuildingBlocks.Domain;
 using Catalog.Domain.Products;
 
 namespace Catalog.Domain.Sales.Events;
+
 public sealed record SaleGeneratedDomainEvent(
     Guid DomainEventId,
     ProductId ProductId,
