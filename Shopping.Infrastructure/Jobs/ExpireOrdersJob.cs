fd7a3f5ced838e0d8d769e7eb8fcae303ad8d2f3
Danilo Usuga <daniusugaj@gmail.com>
2024-02-04 10:43:51 -0500

    Adding unit of work and order repository services to persist data

--- Shopping.Infrastructure/Jobs/ExpireOrdersJob.cs
+++ Shopping.Infrastructure/Jobs/ExpireOrdersJob.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Quartz;
+using Shopping.Application.Common;
+using Shopping.Domain.Orders;
 
 namespace Shopping.Infrastructure.Jobs;
 
@@ -9,11 +11,15 @@ internal sealed class ExpireOrdersJob : IJob
 {
     private readonly ShoppingDbContext _dbContext;
     private readonly ILogger<ExpireOrdersJob> _logger;
+    private readonly IOrderRepository _orderRepository;
+    private readonly IShoppingUnitOfWork _unitOfWork;
 
-    public ExpireOrdersJob(ShoppingDbContext dbContext, ILogger<ExpireOrdersJob> logger)
+    public ExpireOrdersJob(ShoppingDbContext dbContext, ILogger<ExpireOrdersJob> logger, IOrderRepository orderRepository, IShoppingUnitOfWork unitOfWork)
     {
         _dbContext = dbContext;
         _logger = logger;
+        _orderRepository = orderRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Execute(IJobExecutionContext context)
@@ -22,16 +28,21 @@ internal sealed class ExpireOrdersJob : IJob
             nameof(ExpireOrdersJob),
             DateTime.UtcNow);
 
+        var expirationLimit = DateTime.UtcNow.AddDays(-1);
         var ordersToExpire = await _dbContext
             .Orders
-            .Where(d => d.ConfirmedOn != null && d.PayedOn == null && d.ConfirmedOn >= DateTime.UtcNow.AddDays(-1))
+            .Where(d => d.ConfirmedOn != null && d.PayedOn == null && d.ConfirmedOn >= expirationLimit)
             .Take(20)
             .ToListAsync();
 
         foreach (var order in ordersToExpire)
         {
             order.Expire(DateTime.UtcNow);
+
+            await _orderRepository.UpdateAsync(order);
         }
+
+        await _unitOfWork.SaveChangesAsync();
     }
 }
 
