552ef3a01efdea0d15dbca02a38032fd0ef48e29
Danilo Usuga <daniusugaj@gmail.com>
2024-02-04 10:44:42 -0500

    Adding new integration event

---
+++ Shopping.IntegrationEvents/PayMadeIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Application.IntegrationEvents;
+
+namespace Shopping.IntegrationEvents;
+
+public sealed record PayMadeIntegrationEvent(
+    Guid IntegrationEventId,
+    Guid PaymentId,
+    Guid OrderId,
+    Guid PayerId,
+    decimal MoneyAmount,
+    Guid ProductId,
+    DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
