c74c15b879f0091d47106bdc5410895ee0fccfd7
Danilo Usuga <daniusugaj@gmail.com>
2024-02-06 17:21:06 -0500

    Using LINQ query and adding product logic

--- Catalog.Application/Ratings/GetAllRatingsByProductId/GetAllRatingsByProductIdQueryHandler.cs
+++ Catalog.Application/Ratings/GetAllRatingsByProductId/GetAllRatingsByProductIdQueryHandler.cs
@@ -1,45 +1,54 @@
-using System.Data;
 using Catalog.Application.Common;
 using Catalog.Application.Ratings.Catalog.Application.Ratings.GetAllRatingsByProductId;
-using Dapper;
+using Catalog.Domain.Products;
+using Catalog.Domain.Products.Errors;
+using Catalog.Domain.Ratings;
 using ErrorOr;
 
 namespace Catalog.Application.Ratings.GetAllRatingsByProductId;
-internal sealed class GetAllRatingsByProductIdQueryHandler 
-    : IQueryRequestHandler<GetAllRatingsByProductIdQuery, ErrorOr<IReadOnlyList<RatingResponse>>>
-{
-    private readonly IDbConnectionFactory _connectionFactory;
 
-    public GetAllRatingsByProductIdQueryHandler(IDbConnectionFactory connectionFactory)
+internal sealed class GetAllRatingsByProductIdQueryHandler : IQueryRequestHandler<GetAllRatingsByProductIdQuery, ErrorOr<IReadOnlyList<RatingResponse>>>
+{
+    private readonly IRatingRepository _ratingRepository;
+    private readonly IProductRepository _productRepository;
+
+    public GetAllRatingsByProductIdQueryHandler(IRatingRepository ratingRepository, IProductRepository productRepository)
     {
-        _connectionFactory = connectionFactory;
+        _ratingRepository = ratingRepository;
+        _productRepository = productRepository;
     }
 
     public async Task<ErrorOr<IReadOnlyList<RatingResponse>>> Handle(GetAllRatingsByProductIdQuery request, CancellationToken cancellationToken)
     {
-        using IDbConnection dbConnection = _connectionFactory.CreateOpenConnection();
-
-        const string sql = 
-            """
-            SELECT TOP(100) PERCENT r.RatingId,
-                r.Rate,
-                r.UserId,
-                r.ProductId,
-                r.CreatedDateTime,
-                r.UpdatedDateTime,
-                r.Feedback
-            FROM Ratings r
-            WHERE r.ProductId = @ProductId
-            """;
-
-         List<RatingResponse> response 
-            = (await dbConnection
-                .QueryAsync<RatingResponse>(sql, new { request.ProductId })).ToList();
+        bool productExists = await _productRepository.ExistsAsync(ProductId.Create(request.ProductId), cancellationToken);
         
-        if (response.Count == 0) 
+        if (productExists is false)
         {
-            return Error.NotFound("Rating.NotFound", "Ratings in the product with the id provided were not found");
-        } 
+            return ProductErrorCodes.NotFound;
+        }
+
+        List<Rating> ratings = await _ratingRepository.GetRatingsByProductIdAsync(ProductId.Create(request.ProductId));
+
+        if (!ratings.Any())
+        {
+            return RatingErrorCodes.NotFound;
+        }
+
+        var response = new List<RatingResponse>();
+
+        foreach (var rating in ratings)
+        {
+            var ratingResponse = new RatingResponse(
+                rating.Id.Value,
+                rating.Rate,
+                rating.UserId,
+                rating.ProductId.Value,
+                rating.CreatedDateTime,
+                rating.UpdatedDateTime,
+                rating.Feedback);
+
+            response.Add(ratingResponse); 
+        }
 
         return response.AsReadOnly();
     } 
