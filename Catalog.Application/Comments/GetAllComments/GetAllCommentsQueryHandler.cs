d1fe8f8d988ef45b2a3b35099f866435d8c49638
Danilo Usuga <daniusugaj@gmail.com>
2024-02-06 17:19:27 -0500

    Adding product logic

--- Catalog.Application/Comments/GetAllComments/GetAllCommentsQueryHandler.cs
+++ Catalog.Application/Comments/GetAllComments/GetAllCommentsQueryHandler.cs
@@ -1,20 +1,30 @@
 using Catalog.Application.Common;
 using Catalog.Domain.Comments;
 using Catalog.Domain.Products;
+using Catalog.Domain.Products.Errors;
 using ErrorOr;
 
 namespace Catalog.Application.Comments.GetAllComments;
 internal sealed class GetAllCommentsQueryHandler : IQueryRequestHandler<GetAllCommentsQuery, ErrorOr<IReadOnlyList<CommentResponse>>>
 {
     private readonly ICommentRepository _commentRepository;
+    private readonly IProductRepository _productRepository;
 
-    public GetAllCommentsQueryHandler(ICommentRepository commentRepository)
+    public GetAllCommentsQueryHandler(ICommentRepository commentRepository, IProductRepository productRepository)
     {
         _commentRepository = commentRepository;
+        _productRepository = productRepository;
     }
 
     public async Task<ErrorOr<IReadOnlyList<CommentResponse>>> Handle(GetAllCommentsQuery query, CancellationToken cancellationToken)
     {
+        bool productExists = await _productRepository.ExistsAsync(ProductId.Create(query.ProductId), cancellationToken);
+
+        if (productExists is false)
+        {
+            return ProductErrorCodes.NotFound;
+        }
+
         List<Comment>? comments = await _commentRepository.GetAllCommentsByProductIdAsync(ProductId.Create(query.ProductId));
 
         if (comments is null)
