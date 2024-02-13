d7be3da112a7bc5a7c3b5727b4460201dc6b2893
Danilo Usuga <daniusugaj@gmail.com>
2024-02-13 17:09:12 -0500

    Adding Stock status and business rule validation in Publish

---
+++ Catalog.Domain/Products/Rules/ProductCannotBePublishedWithNoStockRule.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Domain;
+using Catalog.Domain.Products.Errors;
+using Catalog.Domain.Products.ValueObjects;
+using ErrorOr;
+
+namespace Catalog.Domain.Products.Rules;
+
+internal sealed class ProductCannotBePublishedWithNoStockRule : IBusinessRule
+{
+    private readonly StockStatus _stockStatus;
+
+    public ProductCannotBePublishedWithNoStockRule(StockStatus stockStatus)
+    {
+        _stockStatus = stockStatus;
+    }
+
+    public Error Error => ProductErrorCodes.CannotPublishWithNoStock;
+
+    public bool IsBroken() => _stockStatus == StockStatus.OutOfStock;
+
+    public static string Message => "Product cannot be published with no stock";
+}
---
+++ Catalog.Domain/Products/ValueObjects/StockStatus.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Catalog.Domain.Products.ValueObjects;
+
+public sealed record StockStatus
+{
+    public static StockStatus WithStock => new StockStatus(nameof(WithStock));
+
+    public static StockStatus OutOfStock => new StockStatus(nameof(OutOfStock));
+
+    public string Value { get; private set; }
+
+    public static StockStatus Create(string value)
+    {
+        return new StockStatus(value);
+    }
+
+    private StockStatus(string value)
+    {
+        Value = value;
+    }
+
+    private StockStatus()
+    {
+    }
+}
