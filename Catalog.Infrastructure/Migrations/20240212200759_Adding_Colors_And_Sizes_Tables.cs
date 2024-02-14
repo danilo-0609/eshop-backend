21dcb6330c2c9ca96fdedd88f17af1273fff4bc3
Danilo Usuga <daniusugaj@gmail.com>
2024-02-14 15:17:25 -0500

    Applying migrations to the database

---
+++ Catalog.Infrastructure/Migrations/20240212200759_Adding_Colors_And_Sizes_Tables.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingColorsAndSizesTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Colors",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sizes",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StockStatus",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Colors",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sizes",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockStatus",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Catalog.Infrastructure/Migrations/20240212205842_Adding_Color_And_Size_Tables.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingColorAndSizeTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Colors",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sizes",
+                schema: "catalog",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "Colors",
+                schema: "catalog",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colors", x => new { x.ProductId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Colors_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "catalog",
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sizes",
+                schema: "catalog",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sizes", x => new { x.ProductId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Sizes_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "catalog",
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colors",
+                schema: "catalog");
+
+            migrationBuilder.DropTable(
+                name: "Sizes",
+                schema: "catalog");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Colors",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sizes",
+                schema: "catalog",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
